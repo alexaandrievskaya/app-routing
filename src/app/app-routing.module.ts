75402aa15aecada2a4c2d0806c7a6cd8a2086090
alexa_andri <48593313+alexaandrievskaya@users.noreply.github.com>
2020-11-07 16:19:42 +0200

    UPD: added router link.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {List1Component} from './list1/list1.component';
+import {List2Component} from './list2/list2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list1', component: List1Component},
+  {path: 'list2', component: List2Component},
+  {path: '', redirectTo: 'list1', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { List1Component } from './list1/list1.component';
+import { List2Component } from './list2/list2.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    List1Component,
+    List2Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
