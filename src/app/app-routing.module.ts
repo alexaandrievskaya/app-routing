f95c67b53332618fa8be07157a2e25f5f37f9788
alexa_andri <48593313+alexaandrievskaya@users.noreply.github.com>
2020-11-07 16:51:28 +0200

    UPD: added barrel file.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {List1Component} from './list1/list1.component';
-import {List2Component} from './list2/list2.component';
+
+import { List1Component, List2Component, HomeComponent } from './index';
 
 const routes: Routes = [
   {path: 'list1', component: List1Component},
   {path: 'list2', component: List2Component},
-  {path: '', redirectTo: 'list1', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
 
 @NgModule({
---
+++ src/app/index.ts
@@ -0,0 +1,3 @@
+export {List1Component} from './list1/list1.component';
+export {List2Component} from './list2/list2.component';
+export {HomeComponent} from './home/home.component';
